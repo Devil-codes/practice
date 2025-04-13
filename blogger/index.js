53c9a59940b65ca4210faf9dc863d569ad945c85
Devil-codes <devilgames1999@gmail.com>
2025-04-13 15:38:36 +0530

    additional projects

--- blogger/index.js
+++ blogger/index.js
@@ -9,6 +9,7 @@ const PORT = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
